b9128cdb7fa955886dd5574f5ebccbb9f6f70e25
mofumofuchan <you_oki1@yahoo.co.jp>
2020-09-27 20:31:17 +0900

    Watsonアプリを追加

--- sample_django/settings.py
+++ sample_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'news.apps.NewsConfig',
+    'watson.apps.WatsonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
